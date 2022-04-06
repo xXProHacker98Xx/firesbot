fa9254ae8d72ef3447625b6c3c4ba053201a9c3a
xXProHacker98Xx <67058608+xXProHacker98Xx@users.noreply.github.com>
2022-04-06 22:27:15 +0200

    Upload file

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+);
+
+client.login(process.env.token)
+
+client.on("ready", () => {
+    console.log("Bot ONLINE")
+});
+
+client.on("messageCreate", message => {
+    if (message.content == "!twitch") {
+        message.channel.send("@everyone Fenyx è in live con una nuovissima cacata pazzurdissimerrima:https://www.twitch.tv/zfenyyx")
+    }
+
+    if (message.content == "!spotify") {
+        message.channel.send("Questa è la playlist di fenyx:https://open.spotify.com/playlist/0E0u0bHwzFolFaes7uG41v?si=b91dd9b07111471f")
+    }
+});
