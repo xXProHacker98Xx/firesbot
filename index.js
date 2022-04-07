8abb994150098a7fa9d136f3aa2b0738ca2fd68d
xXProHacker98Xx <67058608+xXProHacker98Xx@users.noreply.github.com>
2022-04-07 17:29:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ client.on("ready", () => {
 
 client.on("messageCreate", message => {
     if (message.content == "!twitch") {
-        message.channel.send("@everyone Fenyx è in live con una nuovissima cacata pazzurdissimerrima:https://www.twitch.tv/zfenyyx")
+        message.channel.send("@everyone Fenyx è in live con una nuovissima cacata pazzurdissimerrima: https://www.twitch.tv/zfenyyx")
     }
 
     if (message.content == "!spotify") {
-        message.channel.send("Questa è la playlist di fenyx:https://open.spotify.com/playlist/0E0u0bHwzFolFaes7uG41v?si=b91dd9b07111471f")
+        message.channel.send("Questa è la playlist di fenyx: https://open.spotify.com/playlist/0E0u0bHwzFolFaes7uG41v?si=b91dd9b07111471f")
     }
 });
