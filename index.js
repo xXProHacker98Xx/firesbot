295f797e50ff08906e2e913f3a262065e6edd1f4
xXProHacker98Xx <67058608+xXProHacker98Xx@users.noreply.github.com>
2022-04-10 10:01:10 +0200

    Update

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const Discord = require("discord.js");
-const express = require("express")
-const app = express()
 const client = new Discord.Client(
     { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]});
 
@@ -13,9 +11,7 @@ client.on("ready", () => {
         url: "https://www.twitch.tv/zfenyyx"
     });
 });
-app.enable("trust proxy")
-app.set("etag", false)
-app.use(express.static(__dirname + "/website"))
+
 client.on("messageCreate", message => {
     if (message.content == "!twitch") {
         message.channel.send("@everyone Fenyx è in live con una nuovissima cacata pazzurdissimerrima: https://www.twitch.tv/zfenyyx")
@@ -25,18 +21,3 @@ client.on("messageCreate", message => {
         message.channel.send("Questa è la playlist di fenyx: https://open.spotify.com/playlist/0E0u0bHwzFolFaes7uG41v?si=b91dd9b07111471f")
     }
 });
-///////////////////////////
-
-
-// dashboard/website ///
-app.use((reg, res, next) => {
-    console.log(`- ${req.method}: ${req.url} ${res.statusCode} ( by: ${req.ip} )`)
-    next()
-})
-
-
-app.get("/", async (req, res) => {
-    res.send("CIAO MONDO")
-})
-
-app.listen(process.env.PORT || 90, () => console.log(`App on port ${process.env.PORT || 90}`))
