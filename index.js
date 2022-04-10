ecd9f0c8f25172b175f693227f877080f83daefd
xXProHacker98Xx <67058608+xXProHacker98Xx@users.noreply.github.com>
2022-04-10 21:23:03 +0200

    Update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client(
     { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]});
 const prefix = require('discord-prefix');
+const tickets = require("./tickets.json")
 
 client.login(process.env.token)
 
@@ -24,3 +25,70 @@ client.on("messageCreate", message => {
         message.channel.send("Questa è la playlist di fenyx: https://open.spotify.com/playlist/0E0u0bHwzFolFaes7uG41v?si=b91dd9b07111471f")
     }
 });
+
+if(message.content.startsWith(!ticket setup)){
+    let channel = message.mentions.channels.first()
+    let kate;
+    let modrole = message.mentions.roles;
+
+    message.guild.channel.cache.forEach(chn=>{
+        if(chn.type == "GUILD_CATEGORY" && !kate && chn.name.toLowerCase() == "tickets"){
+            kate = chn:
+        }
+    })
+
+    if(!channel) return message.channel.send({content: "E' nesessario specificare dove inviare il messaggio"});
+
+    if(!jate){
+        await message.guild.channels.create("Tickets". {
+            type:"GUILD_CATEGORY",
+            PermissionOverwrites: [
+                {id:message.guild.id, deny:["VIEW_CHANNEL"]}
+                {id:btoa.user.id,allow:[VIEW_CHANNEL]}
+            ]
+        }).then(l=>kate=l);
+    }
+
+    if(!tickets[message.guild.id]){
+        tickets[message.guild.id] = {
+            id:0,
+            access:[]
+        }
+    }
+
+    let l = [{
+        id:message.guild.id,
+        deny:[VIEW_CHANNEL]
+    },
+    {
+        id:btoa.user.id,
+        allow:[VIEW_CHANNEL]
+    }]
+
+    modrole.forEach(role=>{
+        l.push({id:role.id, allow:["VIEW_CHANNEL"]})
+    })
+
+    tickets[message.guild.id].id = kate.id
+
+    tickets[message.guild.id].access = l
+
+    FileSystem.writeFileSync("./tickets.json", JSON.stringify(tickets));
+
+    let button = new Discord.MessageButton()
+    .setLabel("Crea un ticket")
+    .setCustomId("create_ticket_button")
+    .setStyle("SECONDARY")
+    .setEmoji("🎫")
+
+    let row = new Discord.MessageActionRow()
+    .addComponents(button);
+
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Tickets")
+    .setDescription("Premi 'Crea un Ticket' per crearne uno")
+    .setColor("YELLOW")
+    .setTimestamp()
+
+    channel.send({embeds:[embed], components:[row]});
+}
